92aa41e81a1c67518f044640f0b60acedfef9968
CHIRANJEEV1707 <60033114+CHIRANJEEV1707@users.noreply.github.com>
2020-06-05 18:58:49 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,14 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    //this.image = loadImage("sprites/wood1.png");
+  }
+display(){
+ // var pos=this.body.position;
+ // rectMode(CENTER);
+  fill("black");
+  super.display();
+  //rect(pos.x,poos.y,this.width,this.height);
+}
+
+};
